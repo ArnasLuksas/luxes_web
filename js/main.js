49f2577410365593a31f311d321aba8268e92bdc
Arnas <arnasluksas@gmail.com>
2020-12-19 23:36:49 +0000

    responsive and nav
    added:
    1. responsive design(mobile, tablet, desktop)
    2. toggle menu(but have issue, when  clicking back on burger the body does't came back to scroll mode)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
-//mobile menu
-var burger = document.querySelector(".nav-burger"); 
-var list = document.querySelector(".list");
+// mobile menu
 
-   burger.addEventListener('click', ()=>{
-       list.classList.toggle("open");
-   });
+// var burger = document.querySelector(".nav-burger");
+// var list = document.querySelector(".list");
+// var body = document.querySelector("body");
+
+// burger.addEventListener("click", () => {
+//   list.classList.toggle("open");
+
+//   if (list) {
+//     document.querySelector("body").style.overflow = "hidden";
+//     console.log(list);
+//   } else {
+//     document.querySelector("body").style.overflow = "scroll";
+//   }
+// });
+
+// var burger = document.querySelector(".nav-burger");
+// var list = document.querySelector(".list");
+// var body = document.querySelector("body");
+
+// burger.addEventListener("click", () => {
+//   list.classList.toggle("open");
+//   if (body) {
+//     body.style.overflow = "hidden";
+//     console.log(body);
+//   } else if (body) {
+//     body.style.overflow = "scroll";
+//   }
+// });
+// location.reload();
+
+var burger = document.querySelector(".nav-burger");
+var list = document.querySelector(".list");
+var body = document.querySelector("body");
+
+burger.addEventListener("click", () => {
+  list.classList.toggle("open");
+  body.style.overflow = "hidden";
+  if (body) {
+    body.style.overflow = "hidden";
+  }
+});
+// onclick = "location.reload();";
+//     body.reload();
