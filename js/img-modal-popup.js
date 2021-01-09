3cd6ff0c0c6c50e50e79defc2996ffa1f00cb4ec
Arnas <arnasluksas@gmail.com>
2021-01-09 23:58:34 +0000

    added image modal popup effect
    image modal popup effect

---
+++ js/img-modal-popup.js
@@ -0,0 +1,32 @@
+//image modal popup
+
+// all images inside the image modal content class
+const lightboxImage = document.querySelectorAll(".object img");
+// selecting image modal popup div
+const modalPopup = document.querySelector(".image-modal-popup");
+//selecting body (using var bacause with const I get error about body(because of safari))
+var body = document.querySelector("body");
+
+// dynamically selects all elements inside modal popup
+const modalElement = (element) =>
+  document.querySelector(`.image-modal-popup ${element}`);
+
+// closes modal on clicking anywhere and adds overflow back
+document.addEventListener("click", () => {
+  body.style.overflow = "auto";
+  modalPopup.style.display = "none";
+});
+// loops over each modal content img and adds click event functionality
+lightboxImage.forEach((img) => {
+  //   const data = img.dataset;
+  img.addEventListener("click", (e) => {
+    body.style.overflow = "hidden";
+    e.stopPropagation();
+    modalPopup.style.display = "block";
+    // modalElement("h1").innerHTML = data.title;
+    // modalElement("p").innerHTML = data.description;
+    // modalElement("a").href = data.url;
+    // modalElement(".secondary-link").href = data.repo;
+    modalElement("img").src = img.src;
+  });
+});
